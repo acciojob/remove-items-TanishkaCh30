4938e7abd162274dc37ae50719158bf1968c25b3
TanishkaCh30 <137798637+TanishkaCh30@users.noreply.github.com>
2025-02-07 10:44:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+let btn = document.getElementById("button");
+let select = document.getElementById("colorSelect");
+
+let options = select.options;
+
+
+function deleteItems(){
+	let selectedOption = options[select.selectedIndex];
+	if (selectedOption) {
+    colorSelect.remove(selectedOption.index);
+}
+	
+}
+
+
+btn.addEventListener("click" , deleteItems);
